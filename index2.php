5c65c8e60e25a5f4575c7e78d000221b2e4be96b
LAPTOP-U3NBCKVF\Jari Venhuis <jari.venhuis@gmail.com>
2018-04-28 17:39:14 +0200

    qq

---
+++ index2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <title>Kan Jeffrey een korte broek aan</title>
+</head>
+<body class="body">
+<div class="container">
+  <div class="text-center">
+    <div class="image pulseit"></div>
+  <div>
+    <audio controls="controls" autoplay>
+  <source src="muziekie.mp3" type="audio/mp3" />
+</audio>
+  <h1 class="flashit text-center" style="color:red;">Ja hoor Jeffrey Kent kan altijd een korte broek aan!!!</h1>
+</div>
+</body>
+</html>
